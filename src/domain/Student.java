971912fd002ccd0fc4351b84c4d5677157c5ac28
mahdipanahimpui <79366375+mahdipanahimpui@users.noreply.github.com>
2023-01-02 16:33:05 +0330

    do some improvement and delete duplicate class

--- src/domain/Student.java
+++ src/domain/Student.java
@@ -1,21 +1,25 @@
 package domain;
+import domain.exceptions.EnrollmentRulesViolationException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 
 public class Student {
 	private String id;
 	private String name;
 
 	static class CourseSection {
-        CourseSection(Course course, int section) {
-            this.course = course;
-            this.section = section;
-        }
-        Course course;
-	    int section;
-    }
+		CourseSection(Course course, int section) {
+			this.course = course;
+			this.section = section;
+		}
+
+		Course course;
+		int section;
+	}
+
 	private Map<Term, Map<Course, Double>> transcript;
 	private List<CourseSection> currentTerm;
 
@@ -25,9 +29,9 @@ public class Student {
 		this.transcript = new HashMap<>();
 		this.currentTerm = new ArrayList<>();
 	}
-	
-	public void takeCourse(Course c, int section) {
-		currentTerm.add(new CourseSection(c, section));
+
+	public void takeCourse(Course course, int section) {
+		currentTerm.add(new CourseSection(course, section));
 	}
 
 	public Map<Term, Map<Course, Double>> getTranscript() {
@@ -35,24 +39,54 @@ public class Student {
 	}
 
 	public void addTranscriptRecord(Course course, Term term, double grade) {
-	    if (!transcript.containsKey(term))
-	        transcript.put(term, new HashMap<>());
-	    transcript.get(term).put(course, grade);
-    }
+		if (!transcript.containsKey(term))
+			transcript.put(term, new HashMap<>());
+		transcript.get(term).put(course, grade);
+	}
 
-    public List<CourseSection> getCurrentTerm() {
-        return currentTerm;
-    }
+	public List<CourseSection> getCurrentTerm() {
+		return currentTerm;
+	}
 
-    public String getId() {
+	public String getId() {
 		return id;
 	}
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public String toString() {
 		return name;
 	}
-}
+
+	public void courseHasBeenPassed(CourseSectionExamDate courseSectionExamDate) throws EnrollmentRulesViolationException {
+		for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+			for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+				if (r.getKey().equals(courseSectionExamDate.getCourse()) && r.getValue() >= 10)
+					throw new EnrollmentRulesViolationException(String.format("The student has already passed %s",
+							courseSectionExamDate.getCourse().getName()));
+			}
+		}
+	}
+
+	public void coursePrerequisitesHasBeenPassed(CourseSectionExamDate courseSectionExamDate) throws EnrollmentRulesViolationException {
+		for (Course pre : courseSectionExamDate.getCourse().getPrerequisites()) {
+			if (!pre.checkStudentPrerequisites(this))
+				throw new EnrollmentRulesViolationException(String.format("The student has not passed %s as a prerequisite of %s",
+						pre.getName(), courseSectionExamDate.getCourse().getName()));
+		}
+	}
+
+		public double getGpa () {
+			double points = 0;
+			int totalUnits = 0;
+			for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+				for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+					points += r.getValue() * r.getKey().getUnits();
+					totalUnits += r.getKey().getUnits();
+				}
+			}
+			return points / totalUnits;
+		}
+	}
